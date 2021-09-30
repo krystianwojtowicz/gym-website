6d41d0a4863624bf9d9e68af9122a7346ebfb60d
Krystian Wójtowicz <60481782+krystianwojtowicz@users.noreply.github.com>
2021-09-30 09:50:58 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        nav: false,
+        dots: true,
+        autoplay: false,
+        autoHeight: true,
+        responsive: {
+            0: {
+                items: 1,
+            }
+        }
+    })
+});
+
+// const progress = document.querySelector('.progress-done');
+// progress.getElementsByClassName.width = progress.getAttribute('data-done') + '%';
+// progress.style.opacity = 1;
